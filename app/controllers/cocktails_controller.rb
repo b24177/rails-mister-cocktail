f8165cf38f32403e1bb9fa4a072fc38af127cf24
Brant Monaghan <monaghanb@gmail.com>
2020-08-22 16:26:43 +0200

    Progress on the new page

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -4,11 +4,21 @@ class CocktailsController < ApplicationController
   end
 
   def show
+    @cocktail = Cocktail.find(params[:id])
   end
 
   def new
+    @cocktail = Cocktail.new
   end
 
   def create
+    @cocktail = Cocktail.new(cocktails_params)
+    @cocktail.save
+  end
+
+  private
+
+  def cocktails_params
+    params.require(:cocktails).permit(:name, :ingredient, :dose)
   end
 end
